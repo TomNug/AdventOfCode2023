5200e22777270fcd02504e57e89f796d1ef06a90
t.nugent <t.nugent@hotmail.co.uk>
2023-12-01 09:10:42 +0000

    Restructuring to allow for 2 parts in one project

--- Day 1 - Trebuchet/Program.cs
+++ Day 1 - Trebuchet/Program.cs
@@ -15,18 +15,24 @@ public class Program
         return Convert.ToInt32(new string(chars));
     }
 
+    public static void Part1Solution(string[] instructions)
+    {
+        int sum = 0;
+        foreach (string instruction in instructions)
+        {
+            sum += ExtractNumber(instruction);
+        }
+        Console.WriteLine(String.Format("Final sum is {0}", sum));
+    }
+
     public static void Main(string[] args)
     {
         string testPath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 1 - Trebuchet\Test.txt";
         string fullPath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 1 - Trebuchet\Full.txt";
         string[] instructions = System.IO.File.ReadAllLines(fullPath);
 
-        int sum = 0;
-        foreach (string instruction in instructions)
-        {
-            sum+= ExtractNumber(instruction);
-        }
-        Console.WriteLine(String.Format("Final sum is {0}", sum));
+        Part1Solution(instructions);
+        
     }
 }
 
