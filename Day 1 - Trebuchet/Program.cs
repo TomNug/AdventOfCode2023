c5f1ca20cad11773bb88c5ea68ed943b84b5f108
t.nugent <t.nugent@hotmail.co.uk>
2023-12-01 08:27:33 +0000

    Add project files.

---
+++ Day 1 - Trebuchet/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void main 
+}
+
+
+string testPath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 1 - Trebuchet\Test.txt";
+string[] instructions = System.IO.File.ReadAllLines(testPath);
+
+foreach (string instruction in instructions)
+{
+    string resultString = Regex.Match(instruction, @"\d+").Value;
+    Console.WriteLine(resultString);
+}
