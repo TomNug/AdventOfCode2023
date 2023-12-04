350402b4d5bdc9ee11196a629bd99a41c1298186
t.nugent <t.nugent@hotmail.co.uk>
2023-12-04 11:47:13 +0000

    Scratch card class to store properties

---
+++ Day 4 - Scratch Cards/ScratchCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4___Scratch_Cards
+{
+    public class ScratchCard
+    {
+        public int Number { get; set; }
+        public HashSet<int> WinningNumbers { get; set; }
+        public HashSet<int> Numbers { get; set; }
+        public ScratchCard(int number, List<int> winningNumbers, List<int> numbers)
+        {
+            Number = number;
+            foreach (int num in winningNumbers)
+                WinningNumbers.Add(num);
+            foreach (int num in numbers)
+                Numbers.Add(num);
+        }
+    }
+}
