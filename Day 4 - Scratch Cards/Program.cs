1502bcf39470451de3b2185c064af039e8491200
t.nugent <t.nugent@hotmail.co.uk>
2023-12-04 11:43:54 +0000

    Setup for Day 4 part 1

---
+++ Day 4 - Scratch Cards/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Day4
+{
+    public class Day4ScratchCards
+    {
+        public static void Part1Solution(string[] instructions)
+        {
+            Console.WriteLine("\n%%% Part 1 %%%");
+            Console.WriteLine(String.Format("The total points is {0}", 5));
+        }
+
+        public static void Part2Solution(string[] instructions)
+        {
+            Console.WriteLine("\n%%% Part 2 %%%");
+            Console.WriteLine(String.Format("The sum of all gear ratios is {0}", 1));
+        }
+        public static void Main(string[] args)
+        {
+            string samplePath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 4 - Scratch Cards\Sample.txt";
+            string fullPath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 3 - Gears\Full.txt";
+            string[] instructions = System.IO.File.ReadAllLines(samplePath);
+            Part1Solution(instructions);
+            Part2Solution(instructions);
+        }
+    }
+}
+
+
---
+++ Unit Tests/Day4UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using Day3;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests
+{
+    public class Day4UnitTests
+    {
+        
+    }
+}
