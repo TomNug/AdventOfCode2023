2ae8e945c793e64d6302dd1fd86d7547109f61eb
t.nugent <t.nugent@hotmail.co.uk>
2023-12-08 16:46:43 +0000

    File setup

---
+++ Day 8 - Haunted Wasteland/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day8
+{
+    public class Program
+    {
+
+        public static void Part1Solution(string[] instructions)
+        {
+            Console.WriteLine("\n%%% Part 1 %%%");
+
+            Console.WriteLine(String.Format("The total winnings are {0}", 5));
+        }
+
+        public static void Part2Solution(string[] instructions)
+        {
+            Console.WriteLine("\n%%% Part 2 %%%");
+            Console.WriteLine(String.Format("The total winnings are {0}", 5));
+        }
+        public static void Main(string[] args)
+        {
+
+
+            string samplePath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 7 - Camel Poker\Sample.txt";
+            string fullPath = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\Day 7 - Camel Poker\Full.txt";
+            string[] instructions = System.IO.File.ReadAllLines(fullPath);
+            Part1Solution(instructions);
+            Part2Solution(instructions);
+        }
+    }
+}
+
+
---
+++ Unit Tests/Day8UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Day8;
+
+namespace Unit_Tests
+{
+    public class Day8UnitTests
+    {
+    }
+}
