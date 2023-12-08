f1b1766838456137857c3b4ae254689f5e6a953e
t.nugent <t.nugent@hotmail.co.uk>
2023-12-08 11:53:02 +0000

    Custom CompareTo

--- Day 7 - Camel Poker/Hand.cs
+++ Day 7 - Camel Poker/Hand.cs
@@ -18,7 +18,7 @@ namespace Day7
         }
 
         // Dictionary to store the values of each hand
-        public static readonly Dictionary<string, int> handValues = new Dictionary<string, int>
+        public static readonly Dictionary<string, int> handStrength = new Dictionary<string, int>
         {
             { "Five of a kind", 6 },
             { "Four of a kind", 5 },
@@ -27,6 +27,25 @@ namespace Day7
             { "Two pair", 2 },
             { "One pair", 1 },
             { "High card", 0 },
+        };
+
+        // Dictionary to store the values of each card
+        public static readonly Dictionary<char, int> cardStrength = new Dictionary<char, int>
+        {
+            { 'A', 13 },
+            { 'K', 12 },
+            { 'Q', 11 },
+            { 'J', 10 },
+            { 'T', 9 },
+            { '9', 8 },
+            { '8', 7 },
+            { '7', 6 },
+            { '6', 5 },
+            { '5', 4 },
+            { '4', 3 },
+            { '3', 2 },
+            { '2', 1 },
+            { '1', 0 },
         };
 
         // Reads the string representing the hard
@@ -43,7 +62,40 @@ namespace Day7
         }
         public int CompareTo(Hand? other)
         {
-            throw new NotImplementedException();
+            int thisScore = DetermineHand();
+            int otherScore = other.DetermineHand();
+            if (thisScore > otherScore)
+                return 1;
+            else if (thisScore < otherScore)
+                return -1;
+            else
+            {
+                // Are tied on quality of hand
+                // Now run card by card comparison
+                for(int i = 0; i < this.cards.Length; i++)
+                {
+                    int thisCard = Day7.Hand.cardStrength[cards[i]];
+                    int otherCard = Day7.Hand.cardStrength[other.cards[i]];
+                    if (thisCard > otherCard)
+                        return 1;
+                    else if (thisCard < otherCard)
+                        return -1;
+                }
+                // Shouldn't reach here (know this from problem description)
+                return 0;
+            }
+        }
+
+        // Overload the > operator
+        public static bool operator >(Hand hand1, Hand hand2)
+        {
+            return hand1.CompareTo(hand2) > 0;
+        }
+
+        // Overload the < operator (optional)
+        public static bool operator <(Hand hand1, Hand hand2)
+        {
+            return hand1.CompareTo(hand2) < 0;
         }
 
         // Returns the number repesenting the value of the hand
@@ -59,19 +111,19 @@ namespace Day7
             frequencyInts.Sort((a, b) => b.CompareTo(a));
 
             if (frequencyInts[0] == 5)
-                return handValues["Five of a kind"];
+                return handStrength["Five of a kind"];
             if (frequencyInts[0] == 4)
-                return handValues["Four of a kind"];
+                return handStrength["Four of a kind"];
             if (frequencyInts[0] == 3 && frequencyInts[1] == 2)
-                return handValues["Full house"];
+                return handStrength["Full house"];
             if (frequencyInts[0] == 3)
-                return handValues["Three of a kind"];
+                return handStrength["Three of a kind"];
             if (frequencyInts[0] == 2 && frequencyInts[1] == 2)
-                return handValues["Two pair"];
+                return handStrength["Two pair"];
             if (frequencyInts[0] == 2)
-                return handValues["One pair"];
+                return handStrength["One pair"];
             else
-                return handValues["High card"];
+                return handStrength["High card"];
         }
     }
 }
--- Unit Tests/Day7UnitTests.cs
+++ Unit Tests/Day7UnitTests.cs
@@ -40,37 +40,37 @@ namespace Unit_Tests
             yield return new object[]
             {
                 "32T3K",
-                Day7.Hand.handValues["One pair"]
+                Day7.Hand.handStrength["One pair"]
             };
             yield return new object[]
             {
                 "22222",
-                Day7.Hand.handValues["Five of a kind"]
+                Day7.Hand.handStrength["Five of a kind"]
             };
             yield return new object[]
             {
                 "44441",
-                Day7.Hand.handValues["Four of a kind"]
+                Day7.Hand.handStrength["Four of a kind"]
             };
             yield return new object[]
             {
                 "33312",
-                Day7.Hand.handValues["Three of a kind"]
+                Day7.Hand.handStrength["Three of a kind"]
             };
             yield return new object[]
             {
                 "44455",
-                Day7.Hand.handValues["Full house"]
+                Day7.Hand.handStrength["Full house"]
             };
             yield return new object[]
             {
                 "33166",
-                Day7.Hand.handValues["Two pair"]
+                Day7.Hand.handStrength["Two pair"]
             };
             yield return new object[]
             {
                 "12345",
-                Day7.Hand.handValues["High card"]
+                Day7.Hand.handStrength["High card"]
             };
         }
         [Theory]
@@ -88,7 +88,49 @@ namespace Unit_Tests
         }
 
 
+        public static IEnumerable<object[]> Day7_Hand_CompareTo_Data()
+        {
+            yield return new object[]
+            {
+                "11111", // Wins with five of a kind
+                "22221",
+                true
+            };
+            yield return new object[]
+            {
+                "12345",
+                "44433", // Wins with full house
+                false
+            };
+            yield return new object[]
+            {
+                "22233",
+                "22244", // Wins on tie
+                false
+            };
+            yield return new object[]
+            {
+                "12345",
+                "12346", // Wins on tie
+                false
+            };
+        }
+        [Theory]
+        [MemberData(nameof(Day7_Hand_CompareTo_Data))]
+        public void Day7_Hand_CompareTo(string handString1 , string handString2,
+            bool expected)
+        {
+            //Arrange
 
+            //Act
+            var hand1 = new Day7.Hand(handString1);
+            var hand2 = new Day7.Hand(handString2);
+            bool comparison = hand1 > hand2;
+
+            var result = (comparison == expected);
+            //Assert
+            result.Should().BeTrue();
+        }
 
     }
 }
