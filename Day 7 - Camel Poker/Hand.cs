2cf10346b85c3ae15a918fcd9e42aeeb8a863f18
t.nugent <t.nugent@hotmail.co.uk>
2023-12-08 11:14:11 +0000

    Counts cards in hand

---
+++ Day 7 - Camel Poker/Hand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public class Hand: IComparable<Hand>
+    {
+        public string cards { get; set; }
+        public Dictionary<char, int> cardFrequencies { get; set; }
+        public Hand(string cards)
+        {
+            this.cards = cards;
+            cardFrequencies = new Dictionary<char, int>();
+            CountCards();
+        }
+
+        // Dictionary to store the values of each hand
+        private static readonly Dictionary<string, int> handValues = new Dictionary<string, int>
+        {
+            { "Five of a kind", 6 },
+            { "Four of a kind", 5 },
+            { "Full house", 4 },
+            { "Three of a kind", 3 },
+            { "Two pair", 2 },
+            { "One pair", 1 },
+            { "High card", 0 },
+        };
+
+        // Reads the string representing the hard
+        // Stores the frequency of each card
+        public void CountCards()
+        {
+            foreach(char c in cards)
+            {
+                if (cardFrequencies.ContainsKey(c))
+                    cardFrequencies[c]++;
+                else
+                    cardFrequencies.Add(c, 1);
+            }
+        }
+        public int CompareTo(Hand? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Returns the number repesenting the value of the hand
+        public int DetermineHand()
+        {
+            int highestFrequency = 0;
+
+            foreach(char c in cards)
+            {
+
+            }
+            return 5;
+        }
+    }
+}
--- Unit Tests/Day7UnitTests.cs
+++ Unit Tests/Day7UnitTests.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Day6;
+using Day7;
 using FluentAssertions;
 
 namespace Unit_Tests
 {
     public class Day7UnitTests
     {
-        
+        public static IEnumerable<object[]> Day7_Hand_CountCards_Data()
+        {
+            yield return new object[]
+            {
+                "32T3K",
+                new Dictionary<char, int>{{'3',2 }, {'2',1 }, {'T',1 }, {'K',1 } }
+            };
+        }
+        [Theory]
+        [MemberData(nameof(Day7_Hand_CountCards_Data))]
+        public void Day7_Hand_CountCards(string handString,
+            Dictionary<char, int> expected)
+        {
+            //Arrange
+
+            //Act
+            var hand = new Day7.Hand(handString);
+            var resultDictionary = hand.cardFrequencies;
+            //Assert
+            resultDictionary.Should().BeEquivalentTo(expected);
+        }
 
     }
 }
