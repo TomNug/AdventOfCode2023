dd6b9c9fa711084c73640463c629208dad862298
t.nugent <t.nugent@hotmail.co.uk>
2023-12-09 13:14:13 +0000

    2* solution

---
+++ Day 8 - Haunted Wasteland/LCMHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    public class LCMHelper
+    {
+        public static long CalculateLCMFromListsOfFactors(List<List<int>> factorsForEachPath)
+        {
+            Dictionary<int, int> uniqueFactorsAndHighestPower = new Dictionary<int, int>();
+
+            // Examine each path's factors
+            foreach (List<int> factors in factorsForEachPath)
+            {
+                Dictionary<int, int> factorsAndFrequency = new Dictionary<int, int>();
+                // Examine each factor to find factors and frequencies
+                foreach (int factor in factors)
+                {
+                    // Factor already included
+                    if (factorsAndFrequency.ContainsKey(factor))
+                        factorsAndFrequency[factor]++;
+                    else
+                        factorsAndFrequency.Add(factor, 1);
+                }
+
+                // Update overall unique factors and highest powers
+                foreach (KeyValuePair<int, int> factorFreq in factorsAndFrequency)
+                {
+                    // Not unique
+                    if (uniqueFactorsAndHighestPower.ContainsKey(factorFreq.Key))
+                    {
+                        // Check if power can be raised
+                        if (factorFreq.Value > uniqueFactorsAndHighestPower[factorFreq.Key])
+                        {
+                            uniqueFactorsAndHighestPower[factorFreq.Key] = factorFreq.Value;
+                        }
+                    }
+                    // Unique
+                    else
+                    {
+                        uniqueFactorsAndHighestPower[factorFreq.Key] = factorFreq.Value;
+                    }
+                }
+            }
+
+            // construct LCM
+            long lcm = 1;
+            foreach (KeyValuePair<int, int> pair in uniqueFactorsAndHighestPower)
+            {
+                lcm *= Convert.ToInt32(Math.Pow(pair.Key, pair.Value));
+            }
+
+
+
+            return lcm;
+
+
+
+        }
+    }
+}
--- Day 8 - Haunted Wasteland/Program.cs
+++ Day 8 - Haunted Wasteland/Program.cs
@@ -1,28 +1,12 @@
 ﻿using System.Text;
 using System.Text.RegularExpressions;
+using Day8;
 
 namespace Day8
 {
     public class Program
     {
-        public static Dictionary<string, Node> ParseInstructions(string[] instructions)
-        {
-            Dictionary<string, Node> result = new Dictionary<string, Node>();
-
-            for (int line=2; line<instructions.Length; line++)
-            {
-                string pattern = @"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)";
-                Match match = Regex.Match(instructions[line], pattern);
-                string name = match.Groups[1].Value;
-                string left = match.Groups[2].Value;
-                string right = match.Groups[3].Value;
-
-                Node newNode = new Node(left, right);
-                result.Add(name, newNode);
-            }
-            return result;
-        }
-
+        // Traverses the desert for part 1
         public static int TraverseDesert(string turns, Dictionary<string, Node> nodes)
         {
             int steps = 0;
@@ -44,17 +28,84 @@ namespace Day8
                 // Move to next instruction
                 turnsIndex = (turnsIndex + 1) % turns.Length;
             }
-
-
-
             return steps;
         }
+        // Used in part 2
+        // Identifies if a string ends in Z
+        public static bool EndsInZ(string name)
+        {
+            if (name[2] != 'Z')
+                    return false;
+            return true;
+        }
+
+        // Calculates the number of steps from A to Z
+        public static int CalculatePathLength(string turns, Dictionary<string, Node> nodes, string node)
+        {
+            // Number of steps taken along route
+            int steps = 0;
+            // Current turn (L or R) in pattern
+            int turnsIndex = 0;
+
+            while (!EndsInZ(node))
+            {
+                // Taking a step
+                steps++;
+
+                // Turn to take
+                char turnToTake = turns[turnsIndex];
+
+                // Turn
+                node = nodes[node].Navigate(turnToTake);
+
+                // Move to next instruction
+                turnsIndex = (turnsIndex + 1) % turns.Length;
+            }
+            return steps;
+        }
+        
+        // Takes a list of integers
+        // Returns LCM
+        public static long CalculatePathAlignment(List<int> pathLengths)
+        {
+            //pathLengths = new List<int> { 12, 18 }; // TEST, DELETE AFTER
+            pathLengths.Sort();
+            Day8.PrimeHelper.GeneratePrimes(pathLengths[pathLengths.Count - 1]);
+            List<List<int>> factorsForEachPath = new List<List<int>>();
+
+            foreach(int pathLength in pathLengths)
+            {
+                factorsForEachPath.Add(Day8.PrimeHelper.PrimeFactors(pathLength));
+            }
+
+            long lcm = Day8.LCMHelper.CalculateLCMFromListsOfFactors(factorsForEachPath);
+            return lcm;
+        }
+
+        
+        public static long TraverseDesertPart2(string turns, Dictionary<string, Node> nodes, List<string> currentNodes)
+        {
+            // Stores the path length from A-Z for each starting node
+            // Assuming they loop perfectly, with each loop of an equal length
+            List<int> pathLengths = new List<int>();
+
+            foreach(string node in currentNodes)
+            {
+                pathLengths.Add(CalculatePathLength(turns, nodes, node));
+            }
+
+            long pathAlignment = CalculatePathAlignment(pathLengths);
+
+            return pathAlignment;
+        }
+
+
         public static void Part1Solution(string[] instructions)
         {
             Console.WriteLine("\n%%% Part 1 %%%");
             // First line is the turn sequence
             string patternInput = instructions[0];
-            Dictionary<string, Node> nodes = ParseInstructions(instructions);
+            Dictionary<string, Node> nodes = Day8.ParserHelper.ParseInstructions(instructions);
             int steps = TraverseDesert(patternInput, nodes);
 
             Console.WriteLine(String.Format("You get to ZZZ in {0} steps", steps));
@@ -63,7 +114,14 @@ namespace Day8
         public static void Part2Solution(string[] instructions)
         {
             Console.WriteLine("\n%%% Part 2 %%%");
-            Console.WriteLine(String.Format("The total winnings are {0}", 5));
+            // First line is the turn sequence
+            string patternInput = instructions[0];
+            (Dictionary<string, Node>, List<string>) nodesStarts = Day8.ParserHelper.ParseInstructionsPart2(instructions);
+            long steps = TraverseDesertPart2(patternInput, nodesStarts.Item1, nodesStarts.Item2);
+
+
+
+            Console.WriteLine(String.Format("You get to ZZZ in {0} steps", steps));
         }
         public static void Main(string[] args)
         {
@@ -71,10 +129,13 @@ namespace Day8
             string day = "Day 8 - Haunted Wasteland";
 
             string samplePath = path + day + @"\Sample.txt";
+            string samplePath2 = path + day + @"\Sample2.txt";
             string fullPath = path + day + @"\Full.txt";
-            string[] instructions = System.IO.File.ReadAllLines(fullPath);
-            Part1Solution(instructions);
-            Part2Solution(instructions);
+            string[] instructionsSample = System.IO.File.ReadAllLines(samplePath);
+            string[] instructionsSample2 = System.IO.File.ReadAllLines(samplePath2);
+            string[] instructionsFull = System.IO.File.ReadAllLines(fullPath);
+            Part1Solution(instructionsFull);
+            Part2Solution(instructionsFull);
         }
     }
 }
