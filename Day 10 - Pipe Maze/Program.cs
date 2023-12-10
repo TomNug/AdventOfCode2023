f9a88c98862895d1a032fb47a6b6b624c5949854
t.nugent <t.nugent@hotmail.co.uk>
2023-12-10 14:29:32 +0000

    Parse grid to array

--- Day 10 - Pipe Maze/Program.cs
+++ Day 10 - Pipe Maze/Program.cs
@@ -5,10 +5,18 @@ namespace Day10
 {
     public class Program
     {
-
+        public static char[,] ParseGrid(string[] instructions)
+        {
+            char[,] grid = new char[instructions.Length, instructions[0].Length];
+            for (int i = 0; i < instructions.Length; i++)
+                for (int j = 0; j < instructions[0].Length; j++)
+                    grid[i, j] = (char)(instructions[i][j]);
+            return grid;
+        }
         public static void Part1Solution(string[] instructions)
         {
             Console.WriteLine("\n%%% Part 1 %%%");
+            char[,] grid = ParseGrid(instructions);
             int maxSteps = 5;
             Console.WriteLine(String.Format("The farthest point is {0} steps away", maxSteps));
         }
