bbfac570a8ae88fdd9bb81343910afa1a1a6d56e
t.nugent <t.nugent@hotmail.co.uk>
2023-12-10 15:28:04 +0000

    Can find adjacent nodes. Tested

---
+++ Day 10 - Pipe Maze/TraversalHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    public class TraversalHelper
+    {
+        /// <summary>
+        /// Finds the next step in the look
+        /// </summary>
+        /// <param name="previous">The previous step in the maze, so we don't go backwards</param>
+        /// <param name="current">Current place in the maze</param>
+        /// <param name="grid">Maze</param>
+        /// <returns></returns>
+        public static (int,int) NextStepThroughMaze((int,int) previous, (int,int) current, char[,] grid)
+        {
+            ((int, int), (int, int)) adjacentPositions = FindAdjacentPositions(current, grid);
+
+            if (adjacentPositions.Item1 == previous)
+                return adjacentPositions.Item2;
+            else
+                return adjacentPositions.Item1;
+        }
+        /// <summary>
+        /// From a point on the grid, finds the two positions you can move to
+        /// </summary>
+        /// <param name="current">Current position</param>
+        /// <param name="grid">Grid</param>
+        /// <returns></returns>
+        public static ((int, int), (int, int)) FindAdjacentPositions((int,int) current, char[,] grid)
+        {
+            List<(int, int)> found = new List<(int, int)>();
+
+            int currentRow = current.Item1;
+            int currentCol = current.Item2;
+            // Check above
+            char potential = grid[currentRow - 1, currentCol];
+            if (potential == '7' || potential == '|' || potential == 'F')
+            {
+                found.Add((currentRow - 1, currentCol));
+            }
+            // Check right
+            potential = grid[currentRow, currentCol+1];
+            if (potential == 'J' || potential == '-' || potential == '7')
+            {
+                found.Add((currentRow, currentCol + 1));
+            }
+            
+            if (found.Count < 2)
+            {
+                // Check left
+                potential = grid[currentRow, currentCol - 1];
+                if (potential == 'L' || potential == '-' || potential == 'F')
+                {
+                    found.Add((currentRow, currentCol - 1));
+                }
+                if (found.Count < 2)
+                {
+                    // Check down
+                    potential = grid[currentRow + 1, currentCol];
+                    if (potential == 'J' || potential == '|' || potential == 'L')
+                    {
+                        found.Add((currentRow + 1, currentCol));
+                    }
+                }
+            }
+
+            ((int, int), (int, int)) returnVal = (found[0], found[1]);
+            return returnVal;
+        }
+    }
+}
--- Unit Tests/Day10UnitTests.cs
+++ Unit Tests/Day10UnitTests.cs
@@ -4,10 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Day10;
+using FluentAssertions;
 
 namespace Day10
 {
     public class Day10UnitTests
     {
+        public static IEnumerable<object[]> Day10_TraversalHelper_FindAdjacentPositions_Return2Coords_Data()
+        {
+            yield return new object[]
+            {
+                (1, 1), // From
+                new List<(int,int)> {(1,2), (2,1)} // To
+            };
+            yield return new object[]
+            {
+                (1, 3), // From
+                new List<(int,int)> {(2,3), (1,2)} // To
+            };
+            yield return new object[]
+            {
+                (4,2), // From
+                new List<(int,int)> {(4,1), (3,2)} // To
+            };
+
+        }
+        [Theory]
+        [MemberData(nameof(Day10_TraversalHelper_FindAdjacentPositions_Return2Coords_Data))]
+        public void Day10_TraversalHelper_FindAdjacentPositions_Return2Coords((int,int) coord, List<(int,int)> expected)
+        {
+            //Arrange
+            char[,] grid = new char[,] {    {'.', '.' , '.' , '.' , '.' },
+                                            {'.', 'F' , '-' , '7' , '.' },
+                                            {'.', '|' , '.' , '|' , '.' },
+                                            {'.', '|' , 'F' , 'J' , '.' },
+                                            {'.', 'L' , 'J' , '.' , '.' },
+                                            {'.', '.' , '.' , '.' , '.' }};
+
+            //Act
+            ((int,int),(int,int)) result = Day10.TraversalHelper.FindAdjacentPositions(coord, grid);
+            List<(int, int)> resultAsList = new List<(int, int)>();
+            resultAsList.Add(result.Item1);
+            resultAsList.Add(result.Item2);
+
+            //Assert
+            resultAsList.Should().BeEquivalentTo(expected);
+        }
+
     }
 }
