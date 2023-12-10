5a8fb877585a0eb44b6350658c64ea5f054229ea
t.nugent <t.nugent@hotmail.co.uk>
2023-12-10 14:24:45 +0000

    Files setup

---
+++ Day 10 - Pipe Maze/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day10
+{
+    public class Program
+    {
+
+        public static void Part1Solution(string[] instructions)
+        {
+            Console.WriteLine("\n%%% Part 1 %%%");
+            int maxSteps = 5;
+            Console.WriteLine(String.Format("The farthest point is {0} steps away", maxSteps));
+        }
+
+        public static void Part2Solution(string[] instructions)
+        {
+            Console.WriteLine("\n%%% Part 2 %%%");
+            int maxSteps = 5;
+            Console.WriteLine(String.Format("The farthest point is {0} steps away", maxSteps));
+        }
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\";
+            string day = "Day 10 - Pipe Maze";
+
+            string samplePath = path + day + @"\Sample.txt";
+            string samplePath2 = path + day + @"\Sample2.txt";
+            string fullPath = path + day + @"\Full.txt";
+            string[] instructionsSample = System.IO.File.ReadAllLines(samplePath);
+            //string[] instructionsSample2 = System.IO.File.ReadAllLines(samplePath);
+            string[] instructionsFull = System.IO.File.ReadAllLines(fullPath);
+            Part1Solution(instructionsSample);
+            Part2Solution(instructionsSample);
+        }
+    }
+}
+
+
---
+++ Unit Tests/Day10UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Day10;
+
+namespace Day10
+{
+    public class Day10UnitTests
+    {
+    }
+}
