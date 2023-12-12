398e14c31fc11ae57ee60a849f28afb88f2ec1da
t.nugent <t.nugent@hotmail.co.uk>
2023-12-12 13:29:15 +0000

    1* solution

--- Day 11 - Astronomy/Program.cs
+++ Day 11 - Astronomy/Program.cs
@@ -5,12 +5,82 @@ namespace Day11
 {
     public class Program
     {
- 
+        public static (List<List<char>>, List<(int,int)>) ParseAndExpandGalaxy(string[] instructions)
+        {
+            bool[] rowHasGalaxy = new bool[instructions.Length];
+            bool[] colHasGalaxy = new bool[instructions[0].Length];
+
+            List<List<char>> grid = new List<List<char>>(); 
+            for (int row = 0; row < instructions.Length; row++)
+            {
+                List<char> rowList = new List<char>();
+                for (int col = 0; col < instructions[0].Length; col++)
+                {
+                    char c = instructions[row][col];
+                    rowList.Add(c);
+                    if (c == '#')
+                    {
+                        rowHasGalaxy[row] = true;
+                        colHasGalaxy[col] = true;
+                    }
+                }
+                grid.Add(rowList);
+            }
+
+            // Expand rows based on columns
+            int numAddedColumns = 0;
+            for (int col = 0; col < colHasGalaxy.Length; col++) 
+            {
+                if (!colHasGalaxy[col])
+                {
+                    foreach(List<char> rowList in grid)
+                    {
+                        rowList.Insert(col+ numAddedColumns, '.');
+                        
+                    }
+                    numAddedColumns++;
+                }
+            }
+
+            // Expand columns based on rows
+            int rowExpandedLength = grid[0].Count;
+            List<char> blankRow = new List<char>();
+            for (int i = 0; i < rowExpandedLength; i++)
+            {
+                blankRow.Add('.');
+            }
+            int numAddedRows = 0;
+            for (int row = 0; row < rowHasGalaxy.Length; row++)
+            {
+                if (!rowHasGalaxy[row])
+                {
+                    grid.Insert(row + numAddedRows, blankRow);
+                    numAddedRows++;
+                }
+            }
+            List<(int, int)> galaxies = new List<(int, int)>();
+            for (int row = 0; row < grid.Count; row++)
+            {
+                for (int col = 0; col < grid[0].Count; col++)
+                {
+                    if (grid[row][col] == '#')
+                    {
+                        galaxies.Add((row, col));
+                    }
+                }
+            }
+            return (grid, galaxies);
+        }
         public static void Part1Solution(string[] instructions)
         {
+
             Console.WriteLine("\n%%% Part 1 %%%");
-            
-            Console.WriteLine(String.Format("The farthest point is {0} steps away", 5));
+            (List<List<char>>, List<(int,int)>) returnVal = ParseAndExpandGalaxy(instructions);
+            List<List<char>> grid = returnVal.Item1;
+            List<(int, int)> galaxies = returnVal.Item2;
+            int sum = ShortestPathCalculator.CalculateSumOfShortestPaths(grid, galaxies);
+
+            Console.WriteLine(String.Format("The sum of shortest paths is {0}", sum));
         }
 
         public static void Part2Solution(string[] instructions)
@@ -22,7 +92,7 @@ namespace Day11
         public static void Main(string[] args)
         {
             string path = @"C:\Users\Tom\Documents\Projects\Advent\2023\AdventOfCode2023\";
-            string day = "Day 11 - Astonomy";
+            string day = "Day 11 - Astronomy";
 
             string samplePath = path + day + @"\Sample.txt";
             string samplePath2 = path + day + @"\Sample2.txt";
@@ -30,8 +100,8 @@ namespace Day11
             string[] instructionsSample = System.IO.File.ReadAllLines(samplePath);
             //string[] instructionsSample2 = System.IO.File.ReadAllLines(samplePath);
             string[] instructionsFull = System.IO.File.ReadAllLines(fullPath);
-            Part1Solution(samplePath);
-            Part2Solution(samplePath);
+            Part1Solution(instructionsFull);
+            Part2Solution(instructionsSample);
         }
     }
 }
---
+++ Day 11 - Astronomy/ShortestPathCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    public class ShortestPathCalculator
+    {
+        public static int CalculatePath((int, int) from, (int, int) to)
+        {
+            return Math.Abs(to.Item1-from.Item1) + Math.Abs(to.Item2-from.Item2);
+        }
+        public static int CalculateSumOfShortestPaths(List<List<char>> gridList, List<(int,int)> galaxies)
+        {
+            int[,] shortestPaths = new int[galaxies.Count, galaxies.Count];
+            for (int row = 0; row < shortestPaths.GetLength(0); row++)
+            {
+                for (int col = 0; col < shortestPaths.GetLength(1); col++)
+                {
+                    shortestPaths[row, col] = int.MaxValue;
+                }
+            }
+
+            for (int from = 0; from < galaxies.Count; from++)
+            {
+                for (int to = 0; to < galaxies.Count; to++)
+                {
+                    if (from == to)
+                    {
+                        shortestPaths[from, to] = 0;
+                    }
+                    else
+                    {
+                        int newPath = CalculatePath(galaxies[from], galaxies[to]);
+                        if (newPath < shortestPaths[from, to])
+                        {
+                            shortestPaths[from, to] = newPath;
+                        }
+                    }
+                }
+            }
+
+            int sum = 0;
+            foreach (int dist in shortestPaths)
+                sum += dist; 
+            return sum/2;
+        }
+    }
+}
